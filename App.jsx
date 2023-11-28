a56abc26616db4918feded738a6a1df6c0b23110
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-27 22:39:27 -0600

    figured out front end and dont know how to add to other git repo

---
+++ App.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [pets, setPets] = useState([]);
+  const [searchName, setSearchName] = useState("");
+  const [searchOwner, setSearchOwner] = useState("");
+  const [foundPet, setFoundPet] = useState(null);
+
+  useEffect(() => {
+    // Fetch all pets when the component mounts
+    fetch("http://localhost:8080/api/v1/pets")
+      .then((response) => response.json())
+      .then((data) => setPets(data))
+      .catch((error) => console.error("Error fetching pets:", error));
+  }, []);
+
+  const handleSearchByName = () => {
+    // Fetch pet by name when the button is clicked
+    fetch(`http://localhost:8080/api/v1/pets/${searchName}`)
+      .then((response) => response.json())
+      .then((data) => setFoundPet(data))
+      .catch((error) => {
+        console.error("Error fetching pet by name:", error);
+        setFoundPet(null);
+      });
+  };
+
+  const handleSearchByOwner = () => {
+    // Fetch pet by owner when the button is clicked
+    fetch(`http://localhost:8080/api/v1/pets/owner/${searchOwner}`)
+      .then((response) => response.json())
+      .then((data) => setFoundPet(data))
+      .catch((error) => {
+        console.error("Error fetching pet by owner:", error);
+        setFoundPet(null);
+      });
+  };
+
+  return (
+    <div>
+      <h1>All Pets</h1>
+      <ul>
+        {pets.map((pet) => (
+          <div key={pet.id}>{pet.name}</div>
+        ))}
+      </ul>
+
+      <div>
+        <h2>Search by Name</h2>
+        <input
+          type="text"
+          value={searchName}
+          onChange={(e) => setSearchName(e.target.value)}
+        />
+        <button onClick={handleSearchByName}>Search</button>
+        {foundPet && (
+          <div>
+            <h3>Found Pet</h3>
+            <p>Name: {foundPet.name}</p>
+            <p>Breed: {foundPet.breed}</p>
+            <p>Age: {foundPet.age}</p>
+            <p>Owner: {foundPet.owner}</p>
+            <p>Owner Telephone #: {foundPet.telephone}</p>
+          </div>
+        )}
+      </div>
+
+      <div>
+        <h2>Search by Owner</h2>
+        <input
+          type="text"
+          value={searchOwner}
+          onChange={(e) => setSearchOwner(e.target.value)}
+        />
+        <button onClick={handleSearchByOwner}>Search</button>
+        {foundPet && (
+          <div>
+            <h3>Found Pet</h3>
+            <p>Name: {foundPet.name}</p>
+            <p>Breed: {foundPet.breed}</p>
+            {/* Add more details as needed */}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
